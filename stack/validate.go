5f9474bef11bcd2ed65aa3b25447efdab957e6bc
tsubaki <tsubaki@moon>
2017-06-09 22:52:06 +0900

    Implement validate-template

--- stack/validate.go
+++ stack/validate.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io/ioutil"
 
+	"github.com/aws/aws-sdk-go/aws/session"
+	cf "github.com/aws/aws-sdk-go/service/cloudformation"
+
 	cli "gopkg.in/urfave/cli.v1"
 	yaml "gopkg.in/yaml.v2"
 )
@@ -24,9 +27,32 @@ func Validate(c *cli.Context) {
 		return
 	}
 
+	client := cf.New(session.Must(session.NewSession()))
+
 	list := config["Templates"].([]interface{})
 	for i := 0; i < len(list); i++ {
-		fmt.Println(list[i])
+		fmt.Println()
+
+		name := list[i].(string)
+		fmt.Println(name)
+
+		buf, err := ioutil.ReadFile(name)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		tmpl := string(buf)
+		input := &cf.ValidateTemplateInput{
+			TemplateBody: &tmpl,
+		}
+
+		out, err := client.ValidateTemplate(input)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(out)
+		}
 	}
 
 }
